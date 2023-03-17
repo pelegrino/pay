6963e9b40874b37f7c886570c4f93b5f63bbddcf
Pelegrino <pelegrino@gmail.com>
2023-03-17 10:44:19 -0300

    Começando

---
+++ src/test/java/br/com/pelegrino/pay/PayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pelegrino.pay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
